0dc7f61d735c33f0b70b8b76e709f9d22ad138d9
Dominik Husák <domca.husak@gmail.com>
2023-04-21 19:38:53 +0200

    commit

---
+++ VláknaCvičení/Kovarna.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VláknaCvičení
+{
+    internal class Kovarna
+    {
+        private int maxZeleza;
+        private int aktualniZelezo;
+        private int zelezoNaBrneni;
+        private int zelezoNaMec;
+        private object zamek = new object();
+
+        public Kovarna(int maxZeleza, int aktualniZelezo, int zelezoNaBrneni, int zelezoNaMec)
+        {
+            this.maxZeleza = maxZeleza;
+            this.aktualniZelezo = aktualniZelezo;
+            this.zelezoNaBrneni = zelezoNaBrneni;
+            this.zelezoNaMec = zelezoNaMec;
+        }
+
+        public bool odeberZelezo(int mnozstvi)
+        {
+            lock (zamek)
+            {
+                if (mnozstvi > aktualniZelezo)
+                {
+                    Console.WriteLine("Odebirane mnozstvi: " + mnozstvi + " Nepodarilo se odebrat mnozstvi.");
+                    return false;   
+                }
+                aktualniZelezo -= mnozstvi;
+                Console.WriteLine("Odebrane Mnozstvi: " + mnozstvi + " Podarilo se odebrat mnozstvi.");
+                return true;    
+            }
+        }
+
+        public bool pridejZelezo(int mnozstvi)
+        {
+            lock (zamek)
+            {
+                int noveMnozstvi = aktualniZelezo + mnozstvi;
+                if (noveMnozstvi > maxZeleza)
+                {
+                    Console.WriteLine("Mnozstvi: " + mnozstvi + " Nepridano. Sklad je plny. Na skladu je " + aktualniZelezo);
+                    return false;
+                }
+                aktualniZelezo = noveMnozstvi;
+                Console.WriteLine("Mnozstvi: " + mnozstvi + " Pridano. Na skladu je " + aktualniZelezo);
+                return true;    
+            }
+        }
+
+        public int vyrobMec(int mnozstviMec)
+        {
+            int mnozstviZeleza = mnozstviMec * zelezoNaMec;
+            if (odeberZelezo(mnozstviZeleza))
+            {
+                Console.WriteLine("Na vyrobu " + mnozstviMec + " mecu bylo zpotrebovano " + mnozstviZeleza + " zeleza.");
+                return mnozstviMec;
+            }
+            Console.WriteLine("Na vyrobu " + mnozstviMec + " mecu nebylo dostatek " + mnozstviZeleza + " zeleza.");
+            return 0;
+        }
+
+        public int vyrobBrneni(int mnozstviBrneni)
+        {
+            int mnozstviZeleza = mnozstviBrneni * zelezoNaBrneni;
+            if (odeberZelezo(mnozstviZeleza))
+            {
+                Console.WriteLine("Na vyrobu " + mnozstviBrneni + " brneni bylo zpotrebovano " + mnozstviZeleza + " zeleza.");
+                return mnozstviBrneni;
+            }
+            Console.WriteLine("Na vyrobu " + mnozstviBrneni + " brneni nebylo zpotrebovano " + mnozstviZeleza + " zeleza.");
+            return 0;
+        }
+    }
+}
---
+++ VláknaCvičení/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VláknaCvičení
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kovarna kovarna = new Kovarna(20, 0, 4, 2);
+            /*Console.WriteLine(kovarna.pridejZelezo(30));
+            Console.WriteLine(kovarna.vyrobMec(10));
+            Console.WriteLine(kovarna.pridejZelezo(10));
+            Console.WriteLine(kovarna.vyrobMec(10));
+            Console.WriteLine(kovarna.vyrobMec(4));*/
+
+            int pocetMecu = 0;
+            int pocetBrneni = 0;
+
+            Thread thread1 = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    kovarna.pridejZelezo(4);
+                    Thread.Sleep(100);
+                }
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    pocetMecu += kovarna.vyrobMec(1);
+                    Thread.Sleep(100);
+                }
+            });
+
+            Thread thread3 = new Thread(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    pocetBrneni += kovarna.vyrobBrneni(1);
+
+                }
+            });
+
+
+            thread1.Start();
+            thread2.Start();  
+            thread3.Start();
+            thread1.Join(); 
+            thread2.Join();
+            thread3.Join();
+
+            Console.WriteLine("Mecu bylo vyrobeno " + pocetMecu);
+            Console.WriteLine("Brneni bylo vyrobeno " + pocetBrneni);
+        }
+    }
+}
